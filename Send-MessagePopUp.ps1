8d6252cfab1a597bb9346e248db79f4c64565076
Kevin Kirkpatrick <kevin@vmotioned.com>
2015-01-30 13:25:21 -0500

    declared -ComputerName param to Test-Connection

--- Send-MessagePopUp.ps1
+++ Send-MessagePopUp.ps1
@@ -20,12 +20,12 @@
 	[+] Created
 
 	#TAG:PUBLIC
-		
+
 			GitHub: https://github.com/vScripter
 			Twitter: @vScripter
 			Email: kevin@vmotioned.com
 			Blog: www.vMotioned.com
-	
+
 	[-------------------------------------DISCLAIMER-------------------------------------]
 	 All script are provided as-is with no implicit
 	 warranty or support. It's always considered a best practice
@@ -35,10 +35,10 @@
 	 If you have questions or issues, please reach out/report them on
 	 my GitHub page. Thanks for your support!
 	[-------------------------------------DISCLAIMER-------------------------------------]
-	
+
 .LINK
 	https://github.com/vScripter
-	
+
 #>
 
 [CmdletBinding()]
@@ -46,63 +46,63 @@ param (
 	[Parameter(Position = 0,
 			   Mandatory = $true)]
 	[System.String[]]$ComputerName,
-	
+
 	[Parameter(Position = 1,
 			   Mandatory = $true)]
 	[System.String]$Message,
-	
+
 	[Parameter(Position = 2,
 			   Mandatory = $false)]
 	[System.String]$TimeInSeconds
 )
 
 BEGIN {
-	
+
 	# Setting global EA preference since we are calling a system binary and not a PowerShell script, function or cmdlet that support -ErrorAction
 	$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
-	
+
 } # end BEGIN block
 
 PROCESS {
-	
+
 	foreach ($computer in $ComputerName) {
 		$i++
-		
-		if (Test-Connection $computer -Count 2 -Quiet) {
+
+		if (Test-Connection -ComputerName $computer -Count 2 -Quiet) {
 			try {
-				
+
 				Write-Verbose -Message "Sending message to $computer"
 				if ($TimeInSeconds) {
-					
+
 					MSG.exe * /SERVER:$computer /TIME:$TimeInSeconds $Message
-					
+
 				} else {
-					
+
 					MSG.exe * /SERVER:$computer $Message
-					
+
 				} # end if/else
-				
+
 			} catch {
-				
+
 				Write-Warning -Message "$computer - $_"
-				
+
 			} # end try/catch
-			
+
 		} else {
-			
+
 			Write-Warning -Message "[$computer] - Unreachable via Ping"
-			
+
 		} # if/else
-		
+
 		# Write total progress to progress bar
 		$totalComputers = $ComputerName.Length
 		$percentComplete = [int](($i / $totalComputers) * 100)
 		Write-Progress -Activity "Working..." -CurrentOperation "$percentComplete% Complete" -Status "Percent Complete" -PercentComplete $percentComplete
-		
+
 	} # foreach $computer
-	
+
 } # end PROCESS block
 
 END {
-	
+
 } # end END block
