bcd90ada248b883ad43e86e0cd17a758f99e13df
Kevin Kirkpatrick <kevin@vmotioned.com>
2015-02-10 23:44:42 -0500

    minor refactor of Get-PendingReboot.ps1

--- Get-PendingReboot.ps1
+++ Get-PendingReboot.ps1
@@ -1,7 +1,6 @@
 ﻿<#
 .SYNOPSIS
     Gets the pending reboot status on a local or remote computer.
-
 .DESCRIPTION
     This function will query the registry on a local or remote computer and determine if the
     system is pending a reboot, from either Microsoft Patching or a Software Installation.
@@ -12,28 +11,28 @@
     CBServicing = Component Based Servicing (Windows 2008)
     WindowsUpdate = Windows Update / Auto Update (Windows 2003 / 2008)
     PendFileRename = PendingFileRenameOperations (Windows 2003 / 2008)
-
+.INPUTS
+	System.String
+.OUTPUTS
+	System.Management.Automation.PSCustomObject
 .PARAMETER ComputerName
     A single Computer or an array of computer names.  The default is localhost ($env:COMPUTERNAME).
-
 .PARAMETER ErrorLog
     A single path to send error data to a log file.
-
 .EXAMPLE
-    PS C:\> Get-PendingReboot -ComputerName (Get-Content C:\ServerList.txt) | Format-Table -AutoSize
+    .\Get-PendingReboot.ps1 -ComputerName (Get-Content C:\ServerList.txt) | Format-Table -AutoSize
 
     Computer CBServicing WindowsUpdate PendFileRename  RebootPending
     -------- ----------- ------------- --------------  -------------
-    DC01           False         False                 False
-    DC02           False         False                 False 
-    FS01           False         False                 False 
+    DC01           False         False  		False          False
+    DC02           False         False          False          False
+    FS01           False         False          False          False
 
     This example will capture the contents of C:\ServerList.txt and query the pending reboot
     information from the systems contained in the file and display the output in a table. The
     null values are by design
-
 .EXAMPLE
-    PS C:\> Get-PendingReboot
+    .\Get-PendingReboot.ps1
 
     Computer       : WKS01
     CBServicing    : False
@@ -42,13 +41,11 @@
     RebootPending  : True
 
     This example will query the local machine for pending reboot information.
-
 .EXAMPLE
-    PS C:\> $Servers = Get-Content C:\Servers.txt
-    PS C:\> Get-PendingReboot -Computer $Servers | Export-Csv C:\PendingRebootReport.csv -NoTypeInformation
+    $Servers = Get-Content C:\Servers.txt
+    C:\PS>.\Get-PendingReboot.ps1 -Computer $Servers | Export-Csv C:\PendingRebootReport.csv -NoTypeInformation
 
     This example will create a report that contains pending reboot information.
-
 .LINK
     Component-Based Servicing:
     http://technet.microsoft.com/en-us/library/cc756291(v=WS.10).aspx
@@ -60,16 +57,34 @@
 
 
 .NOTES
-	Author:  Kevin Kirkpatrick
+    Author:  Kevin Kirkpatrick
     Date:    01/30/2015
     PSVer:   2.0/3.0/4.0
-    Updated: 01/30/2015
-    UpdNote: 
-			[-] Removed all of the code that dealt with SCCM queries, since it is not used in my environment. 
-			[+] Replaced the splatting and PSObject creation; now [PSCustomObject] gets assign to a variable and then the variable is called
-			[-] Remove the function definition in the .ps1 file; it is now a script
-	
-	
+    Updated: 02/10/2015
+    Update Notes:
+        [+] Fixed an issue that wasn't properly checking 'Pending Filename Rename Operation'
+        [+] Renamed most of the variabled
+        [+] Updated some syntax formatting
+        [-] Removed logging parameter and associated log options
+
+	#TAG:PUBLIC
+
+			GitHub: https://github.com/vScripter
+			Twitter: @vScripter
+			Email: kevin@vmotioned.com
+			Blog: www.vMotioned.com
+
+	[-------------------------------------DISCLAIMER-------------------------------------]
+	 All script are provided as-is with no implicit
+	 warranty or support. It's always considered a best practice
+	 to test scripts in a DEV/TEST environment, before running them
+	 in production. In other words, I will not be held accountable
+	 if one of my scripts is responsible for an RGE (Resume Generating Event).
+	 If you have questions or issues, please reach out/report them on
+	 my GitHub page. Thanks for your support!
+	[-------------------------------------DISCLAIMER-------------------------------------]
+
+
 	Original Author
 	--------------------------
     Author:  Brian Wilhite
@@ -77,96 +92,98 @@
     Date:    08/29/2012
     PSVer:   2.0/3.0
     Updated: 05/30/2013
-    UpdNote: 
+    UpdNote:
 #>
 
-[CmdletBinding()]
+[CmdletBinding(PositionalBinding = $true)]
 param (
 	[Parameter(Position = 0,
 			   ValueFromPipeline = $true,
 			   ValueFromPipelineByPropertyName = $true)]
-	[Alias("CN", "Computer")]
-	[String[]]$ComputerName = "$env:COMPUTERNAME",
-	
-	[parameter(Mandatory = $false,
-			   Position = 1)]
-	[String]$ErrorLog
+	[Alias('CN', 'Computer')]
+	[String[]]$ComputerName = "$env:COMPUTERNAME"
 )
 
 BEGIN {
-	# Adjusting ErrorActionPreference to stop on all errors, since using [Microsoft.Win32.RegistryKey]
-	# does not have a native ErrorAction Parameter, this may need to be changed if used within another
-	# function.
-	$TempErrAct = $ErrorActionPreference
-	$ErrorActionPreference = "Stop"
-} # End BEGIN Script Block
+
+	<# Adjusting ErrorActionPreference to stop on all errors, since using [Microsoft.Win32.RegistryKey]
+	 does not have a native ErrorAction Parameter, this may need to be changed if used within another
+	 function. #>
+	$defaultEAPref = $ErrorActionPreference
+	$ErrorActionPreference = 'Stop'
+
+} # end BEGIN block
+
 PROCESS {
-	Foreach ($Computer in $ComputerName) {
-		Try {
+
+	foreach ($computer in $ComputerName) {
+
+		try {
+
 			# Setting pending values to false to cut down on the number of else statements
-			$PendFileRename, $Pending = $false, $false
-			
-			# Setting CBSRebootPend to null since not all versions of Windows has this value
-			$CBSRebootPend = $null
-			
+			[bool]$pendingFileRename = $false
+			[bool]$pending = $false
+
+			# Setting cbsRebootPending to null since not all versions of Windows has this value
+			$cbsRebootPending = $null
+
 			# Querying WMI for build version
-			$WMI_OS = Get-WmiObject -Class Win32_OperatingSystem -Property BuildNumber, CSName -ComputerName $Computer
-			
+			$wmiWin32Os = Get-WmiObject -ComputerName $computer -Query 'SELECT BuildNumber,CSName FROM win32_OperatingSystem'
+
 			# Making registry connection to the local/remote computer
-			$RegCon = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey([Microsoft.Win32.RegistryHive]"LocalMachine", $Computer)
-			
+			$regConnection = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey([Microsoft.Win32.RegistryHive]'LocalMachine', $computer)
+
 			# If Vista/2008 & Above query the CBS Reg Key
-			If ($WMI_OS.BuildNumber -ge 6001) {
-				$RegSubKeysCBS = $RegCon.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\").GetSubKeyNames()
-				$CBSRebootPend = $RegSubKeysCBS -contains "RebootPending"
-				
-			} #End If ($WMI_OS.BuildNumber -ge 6001)
-			
+			if ($wmiWin32Os.BuildNumber -ge 6001) {
+
+				$regCbsSubkey = $regConnection.OpenSubKey('SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\').GetSubKeyNames()
+				$cbsRebootPending = $regCbsSubkey -contains 'RebootPending'
+
+			} #End if $wmiWin32Os.BuildNumber
+
 			# Query WUAU from the registry
-			$RegWUAU = $RegCon.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\")
-			$RegWUAURebootReq = $RegWUAU.GetSubKeyNames()
-			$WUAURebootReq = $RegWUAURebootReq -contains "RebootRequired"
-			
+			$regWindowsUpdate = $regConnection.OpenSubKey('SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\')
+			$regWindowsUpdateKeys = $regWindowsUpdate.GetSubKeyNames()
+			$windowsUpdateRebootPending = $regWindowsUpdateKeys -contains 'RebootRequired'
+
 			# Query PendingFileRenameOperations from the registry
-			$RegSubKeySM = $RegCon.OpenSubKey("SYSTEM\CurrentControlSet\Control\Session Manager\")
-			
+			$regPendingFileRename = $regConnection.OpenSubKey('SYSTEM\CurrentControlSet\Control\Session Manager\')
+			$regPFROQuery = $regPendingFileRename.GetValue('PendingFileRenameOperations', $null)
+			$regPFROValue = $regPFROQuery | Out-String
+
 			# Closing registry connection
-			$RegCon.Close()
-			
-			# If any of the variables are true, set $Pending variable to $true
-			If ($CBSRebootPend -or $WUAURebootReq -or $PendFileRename) {
-				$Pending = $true
-				
-			} # End If ($CBS -or $WUAU -or $PendFileRename)
-			
-			
-			$objReboot = [PSCustomObject] @{
-				Computer = $WMI_OS.CSName
-				CBServicing = $CBSRebootPend
-				WindowsUpdate = $WUAURebootReq
-				PendFileRename = $PendFileRename
-				RebootPending = $Pending
-			}
-			$objReboot
-			
+			$regConnection.Close()
+
+			# if any of the variables are true, set $Pending variable to $true
+			if ($cbsRebootPending -or $windowsUpdateRebootPending -or $pendingFileRename) {
+
+				$pending = $true
+
+			} # end if
+
+			$objPendingReboot = [PSCustomObject] @{
+				Computer = $wmiWin32Os.CSName
+				CBServicing = $cbsRebootPending
+				WindowsUpdate = $windowsUpdateRebootPending
+				PendFileRename = $pendingFileRename
+				RebootPending = $pending
+			} # end $objPendingReboot
+
+			$objPendingReboot
+
 		} #End Try
-		
+
 		Catch {
-			Write-Warning "$Computer`: $_"
-			
-			# If $ErrorLog, log the file to a user specified location/path
-			If ($ErrorLog) {
-				Out-File -InputObject "$Computer`,$_" -FilePath $ErrorLog -Append
-				
-			} #End If ($ErrorLog)
-			
-		} #End Catch
-		
-	} #End Foreach ($Computer in $ComputerName)
-	
+
+			Write-Warning "[$computer][ERROR] $_"
+
+		} # End Catch
+
+	} # End Foreach ($Computer in $ComputerName)
+
 } # End PROCESS
 
 END {
 	# Resetting ErrorActionPref
-	$ErrorActionPreference = $TempErrAct
-} # End END
+	$ErrorActionPreference = $defaultEAPref
+} # End END block
